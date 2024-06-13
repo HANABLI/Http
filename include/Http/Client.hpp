4bbf2d31100170432d00c794b4109907236886ac
Hatem Nabli <hatem.nabli@actemium.com>
2024-06-13 16:16:53 +0200

    Feat: this is a Http client, it will be used to parse Http responses received bac from a server and generate Http Responses objects.

--- include/Http/Client.hpp
+++ include/Http/Client.hpp
@@ -9,11 +9,46 @@
  * © 2024 by Hatem Nabli
  */
 
-#include<memory>
+#include <memory>
+#include <string>
+#include <MessageHeaders/MessageHeaders.hpp>
 
 namespace Http {
 
+    /**
+     * This class is used to generate HTTP Responses (for web servers)
+     * and parse HTTP responses received back from web servers.
+     */
     class Client {
+
+    public:
+        struct Response
+        {
+            /**
+             * This is the machine-readable number that describes
+             * the overall status of the Response
+             */
+            unsigned int statusCode;
+            /**
+             * This is the human-readable text that describes
+             * the overall status of the Response.
+             */
+            std::string status;
+
+            /**
+             * This are the messages headers that were included 
+             * in the Response
+             */
+            MessageHeaders::MessageHeaders headers;
+
+            /**
+             * This is the body of the Response, if there is a body.
+             */
+            std::string body;
+
+        };
+        
+    public:
         ~Client();
         Client(const Client&) = delete; // Copy Constructor that creates a new object by making a copy of an existing object. 
         //It ensures that a deep copy is performed if the object contains dynamically allocated resources 
@@ -26,6 +61,44 @@ namespace Http {
         * This is the default constructor
         */
         Client();
+
+        /**
+         * This method parces the given string as a raw Http Response message.
+         * If the string parses correctly, the equivalent Response is returned.
+         * otherwise, nullptr is returned.
+         * 
+         * @param[in] rawResponse
+         *      This is the raw HTTP Response message as a string.
+         * 
+         * @return
+         *      The Response object is returned from the given raw 
+         *      Http Response string.
+         * 
+         * @retval nullptr
+         *      This is returned if the given rawResponse did not parse correctly.
+         */
+        std::shared_ptr< Response > ParseResponse(const std::string& rawResponse);
+
+        /**
+         * This method parces the given string as a raw Http Response message.
+         * If the string parses correctly, the equivalent Response is returned.
+         * otherwise, nullptr is returned.
+         * 
+         * @param[in] rawResponse
+         *      This is the raw HTTP Response message as a string.
+         * 
+         * @param[out] messageEnd
+         *      This is the raw HTTP Response message end
+         * 
+         * @return
+         *      The Response object is returned from the given raw 
+         *      Http Response string.
+         * 
+         * @retval nullptr
+         *      This is returned if the given rawResponse did not parse correctly.
+         */
+        std::shared_ptr< Response > ParseResponse(const std::string& rawResponse, size_t& messageEnd);
+
 
     private:
         /* data */
@@ -44,4 +117,4 @@ namespace Http {
     };
 }
 
-#endif /* HTTP_Client_HPP */
+#endif /* HTTP_CLIENT_HPP */
--- src/Client.cpp
+++ src/Client.cpp
@@ -8,13 +8,105 @@
  */
 
 #include <Http/Client.hpp>
+#include <StringUtils/StringUtils.hpp>
+
+namespace {
+     /**
+     * This is the character sequence corresponding to a carriage return (CR)
+     * followed by a line feed (LF), which officially delimits each
+     * line of an HTTP request.
+     */
+    const std::string CRLF("\r\n");
+
+    /**
+     * @return 
+     *      An indication of whether or not the number was parsed
+     *      successfully is returned
+     */
+    bool ParseSize(
+        const std::string& stringSize,
+        size_t& number 
+    ) {
+        number = 0;
+        for ( auto c : stringSize ) {
+            if (
+                (c < '0')
+                || (c > '9')
+            ) { 
+                return false;
+            }
+            auto previousNumber = number;
+            number *= 10;
+            number += (uint16_t)(c - '0');
+            if (
+                (number / 10) != previousNumber
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+      /**
+     * This method parses the status, statusCode, and protocol identifier
+     * from the given response line.
+     * 
+     * @param[in] response
+     *      This is the response in which to store the parsed status and 
+     *      statusCode.
+     * 
+     * @param[in] responseLine
+     *      This is the raw response line string to parse.
+     * 
+     * @return
+     *      Returns an indication of whether or not the response line
+     *      was successfuly parsed 
+     */
+    bool ParseResponseLine(std::shared_ptr< Http::Client::Response > response, const std::string& responseLine) {
+
+        //Parse the method
+        const auto protocolDelimiter = responseLine.find(' ');
+        if (protocolDelimiter == std::string::npos) {
+            return false;
+        }
+
+        const auto protocol = responseLine.substr(0, protocolDelimiter);
+
+        // Parse the target URI.
+        const auto statusCodeDelimiter = responseLine.find(' ', protocolDelimiter + 1);
+        
+        if (statusCodeDelimiter == std::string::npos) {
+            return false;
+        }
+
+        intmax_t statusCodeAsInt;
+        if (
+            StringUtils::ToInteger(
+                responseLine.substr(protocolDelimiter + 1, statusCodeDelimiter - protocolDelimiter - 1),
+                statusCodeAsInt
+            ) != StringUtils::ToIntegerResult::Success
+        ) {
+            return false;
+        }
+
+        if (statusCodeAsInt > 999) {
+            return false;
+        } else {
+            response->statusCode = (unsigned int)statusCodeAsInt;
+        }
+
+        response->status = responseLine.substr(statusCodeDelimiter + 1);
+
+        return (protocol == "HTTP/1.1");
+    }
+
+}
 
 namespace Http {
 
     /**
      * This contains the private properties of a Client instance
      */
-
     struct Client::Impl
     {
         
@@ -27,4 +119,58 @@ namespace Http {
 
     }
     
+    auto Client::ParseResponse(const std::string& rawResponse)-> std::shared_ptr< Response > {
+        size_t messageEnd;
+        return ParseResponse(rawResponse, messageEnd);
+    }
+
+    auto Client::ParseResponse(const std::string& rawResponse, size_t& messageEnd)-> std::shared_ptr< Response > {
+        const auto response = std::make_shared< Response >();
+        // First, extarct the response line.
+        const auto responseLineEnd = rawResponse.find(CRLF);
+        if (responseLineEnd == std::string::npos) {
+            return nullptr;
+        }
+        const auto responseLine = rawResponse.substr(0, responseLineEnd);
+        if (!ParseResponseLine(response, responseLine)) {
+            return nullptr;
+        }
+        //Second, parse the message headers and identify where the body begins.
+        size_t bodyOffset;
+        size_t headerOffset = responseLineEnd + CRLF.length();
+        if (
+            !response->headers
+                .ParseRawMessage(
+                    rawResponse.substr(headerOffset),
+                    bodyOffset
+                )
+        ) {
+            return nullptr;
+        }
+        // Check for "Content-Length" header, if present, use it to
+        // determine how many characters should be in the body.
+        bodyOffset += headerOffset;
+        const auto maxContentLength = rawResponse.length() - bodyOffset;
+        // If it containt "Content-Length" header, we carefully carve exactly
+        // that number of cahracters out (and bail if we don't have anough) 
+        if (response->headers.HasHeader("Content-Length")) {
+            size_t contentLength;
+            if (!ParseSize(response->headers.GetHeaderValue("Content-Length"), contentLength)) {
+                return nullptr;
+            }
+            if (contentLength > maxContentLength) {
+                return nullptr;
+            } else {
+                response->body = rawResponse.substr(bodyOffset, contentLength);
+                messageEnd = bodyOffset + contentLength;
+            }
+        } else {
+            //Finally, extract the body
+            response->body.clear();
+            messageEnd = bodyOffset;
+        }
+
+        return response;
+    }
+
 }
