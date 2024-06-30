7b062186151d746236b9b6a96143bddcbe9abe6f
Hatem Nabli <hatem.nabli@actemium.com>
2024-06-30 22:41:45 +0200

    Test: testing several validity states of request parsing

--- test/src/ServerTests.cpp
+++ test/src/ServerTests.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <gtest/gtest.h>
+#include <limits>
 #include <Http/Server.hpp>
 #include <Http/ServerTransportLayer.hpp>
 #include <Http/Connection.hpp>
@@ -36,6 +37,12 @@ namespace {
          */
         std::vector< uint8_t > dataReceived;
 
+
+        /**
+         * This flag is set if  the remote close the connection
+         */
+        bool broken = false;
+
         // Methods
 
         // Http::Connection
@@ -57,6 +64,7 @@ namespace {
         }
 
         virtual void Break(bool clean) override {
+            broken = true;
         }
     };
 
@@ -109,6 +117,7 @@ TEST(ServerTests, ServerTests_ParseGetRequest_Test) {
         "\r\n"
     );
     ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(request->validity, Http::Server::Request::Validity::Valid);
     Uri::Uri expectedUri;
     expectedUri.ParseFromString("/hello.txt");
     ASSERT_EQ("GET", request->method);
@@ -149,6 +158,24 @@ TEST(ServerTests, ServerTests_ParsePostRequest_Test) {
     ASSERT_EQ(rawRequest.length() - 2, messageEnd);
 }
 
+TEST(ServerTests, ServerTests_ParseInvalidRequestNoMethod_Test) {
+    Http::Server server;
+    size_t messageEnd;
+    const std::string rawRequest = (        
+        " /hello.txt HTTP/1.1\r\n"
+        "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+        );
+    const auto request = server.ParseRequest(
+        rawRequest,
+        messageEnd
+    );
+    ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(Http::Server::Request::Validity::InvalidRecoverable, request->validity);
+}
+
 TEST(ServerTests, ServerTests_ParseIncompleteBodyRequ_Test) {
     Http::Server server;
     size_t messageEnd;
@@ -186,7 +213,7 @@ TEST(ServerTests, ServerTests_ParseIncompleteMidLineHeadersRequ_Test) {
     size_t messageEnd;
     const std::string rawRequest = ("POST /test HTTP/1.1\r\n"
         "Host: foo.example\r\n"
-        "Content-Type: application/");
+        "Content-Type: application/x-www\r\n");
     const auto request = server.ParseRequest(
         rawRequest,
         messageEnd
@@ -208,7 +235,6 @@ TEST(ServerTests, ServerTests_ParseNoBodyDelimiterRequ_Test) {
     );
     ASSERT_TRUE(request == nullptr);
 }
-
 
 TEST(ServerTests, ServerTests_ParseIncompleteRequestLine_Test) {
     Http::Server server;
@@ -245,9 +271,103 @@ TEST(ServerTests, RequestWithNoContentLengthOrChunkedTransferEncodingHasNoBody) 
         "Hello, World\r\n"
     );
     ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(request->validity, Http::Server::Request::Validity::Valid);
     Uri::Uri expectedUri;
     expectedUri.ParseFromString("/hello.txt");
     ASSERT_TRUE(request->body.empty());
+}
+
+TEST(ServerTests, ServerTests_ParseInvalidRequestNoTarget_Test) {
+    Http::Server server;
+    size_t messageEnd;
+    const std::string rawRequest = (        
+        "GET HTTP/1.1\r\n"
+        "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+    );
+    const auto request = server.ParseRequest(
+        rawRequest,
+        messageEnd
+    );
+    ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(Http::Server::Request::Validity::InvalidRecoverable, request->validity);
+}
+
+TEST(ServerTests, ServerTests_ParseInvalidRequestBadProtocol_Test) {
+    Http::Server server;
+    size_t messageEnd;
+    const std::string rawRequest = (        
+        "GET /hello.txt Foo\r\n"
+        "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+    );
+    const auto request = server.ParseRequest(
+        rawRequest,
+        messageEnd
+    );
+    ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(Http::Server::Request::Validity::InvalidRecoverable, request->validity);
+}
+
+TEST(ServerTests, ServerTests_ParseInvalidRequestDamageHeader_Test) {
+    Http::Server server;
+    size_t messageEnd;
+    const std::string rawRequest = (        
+        "GET /hello.txt HTTP/1.1\r\n"
+        "User-Agent curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+    );
+    const auto request = server.ParseRequest(
+        rawRequest,
+        messageEnd
+    );
+    ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(Http::Server::Request::Validity::InvalidRecoverable, request->validity);
+}
+
+TEST(ServerTests, ServerTests_ParseInvalidRequestBodyExtremelyTooLarge_Test) {
+    Http::Server server;
+    size_t messageEnd = std::numeric_limits< size_t >::max();
+    const std::string rawRequest = (        
+            "GET /hello.txt HTTP/1.1\r\n"
+            "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+            "Host: www.example.com\r\n"
+            "Content-Length: 1000000000000000000000000000000000\r\n"
+            "Accept-Language: en, mi\r\n"
+            "\r\n"
+        );
+    const auto request = server.ParseRequest(
+        rawRequest,
+        messageEnd
+    );
+    ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(Http::Server::Request::Validity::InvalidUnrecoverable, request->validity);
+    ASSERT_EQ(0, messageEnd);
+}
+
+TEST(ServerTests, ServerTests_ParseInvalidRequestBodySligntlysTooLarge_Test) {
+    Http::Server server;
+    size_t messageEnd;
+    const std::string rawRequest = (        
+        "GET /hello.txt HTTP/1.1\r\n"
+        "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Content-Length: 10000001\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+        );
+    const auto request = server.ParseRequest(
+        rawRequest,
+        messageEnd
+    );
+    ASSERT_FALSE(request == nullptr);
+    ASSERT_EQ(Http::Server::Request::Validity::InvalidUnrecoverable, request->validity);
 }
 
 TEST(ServerTests, ServerTests_Mobiliz_Test) {
@@ -404,6 +524,79 @@ TEST(ServerTests, twoClientRequestsInOnePiece) {
     );
 }
 
-TEST(ServerTests, Client){
+TEST(ServerTests, ClientInvalidRequestRecoverable) {
+    auto transport = std::make_shared< MockTransport >();
+    Http::Server Server;
+    (void)Server.Mobilize(transport, 1234);
+    auto connection = std::make_shared< MockConnection >();
+    transport->connectionDelegate(connection);
+    ASSERT_FALSE(connection->dataReceivedDelegate == nullptr);
+    const std::string request(
+        "POST /hello.txt HTTP/1.1\r\n"
+        "User-Agent curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+    );
+    ASSERT_TRUE(connection->dataReceived.empty());
+    connection->dataReceivedDelegate(   
+        std::vector< uint8_t >(
+            request.begin(),
+            request.end()
+        )
+    );
+    const std::string expectedResponse = (
+        "HTTP/1.1 400 Bad Request\r\n"
+        "Content-Length: 13\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n"
+        "BadRequest.\r\n"
+      );
+    ASSERT_EQ(
+        expectedResponse,
+        std::string(
+            connection->dataReceived.begin(),
+            connection->dataReceived.end()
+        )
+    );
+    ASSERT_FALSE(connection->broken);
+}
 
+TEST(ServerTests, ClientInvalidRequestUnrecoverable) {
+    auto transport = std::make_shared< MockTransport >();
+    Http::Server Server;
+    (void)Server.Mobilize(transport, 1234);
+    auto connection = std::make_shared< MockConnection >();
+    transport->connectionDelegate(connection);
+    ASSERT_FALSE(connection->dataReceivedDelegate == nullptr);
+    const std::string request(
+        "POST /hello.txt HTTP/1.1\r\n"
+        "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n"
+        "Host: www.example.com\r\n"
+        "Content-Length: 1300000000000000000000000000\r\n"
+        "Accept-Language: en, mi\r\n"
+        "\r\n"
+    );
+    ASSERT_TRUE(connection->dataReceived.empty());
+    connection->dataReceivedDelegate(   
+        std::vector< uint8_t >(
+            request.begin(),
+            request.end()
+        )
+    );
+    const std::string expectedResponse = (
+        "HTTP/1.1 400 Bad Request\r\n"
+        "Content-Length: 13\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n"
+        "BadRequest.\r\n"
+      );
+    ASSERT_EQ(
+        expectedResponse,
+        std::string(
+            connection->dataReceived.begin(),
+            connection->dataReceived.end()
+        )
+    );
+    ASSERT_TRUE(connection->broken);
 }
